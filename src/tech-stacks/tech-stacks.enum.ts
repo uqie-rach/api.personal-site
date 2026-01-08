f6bb45554e9905e59d558d46961f103f8410c236
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2026-01-08 14:11:22 +0700

    feat: add tech stacks enums for category and proficiency

---
+++ src/tech-stacks/tech-stacks.enum.ts
@@ -0,0 +1,17 @@
+export enum category {
+  FE = 'frontend',
+  BE = 'backtend',
+  DevOps = 'devOps',
+  Other = 'other',
+  Utility = 'utility',
+  Test = 'testing',
+  Design = 'design',
+  DB = 'database',
+}
+
+export enum proficiency {
+  Beg = 'beginner',
+  Adv = 'advanced',
+  Int = 'intermediate',
+  Exp = 'Expert',
+}
