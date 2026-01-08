81de421cf46fb3f3300538ca0e721f2f86859e52
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2026-01-08 12:21:21 +0700

    feat: enhance user seeding with role and status entities, and add user factory for random user generation

--- src/database/seeds/relational/user/user-seed.module.ts
+++ src/database/seeds/relational/user/user-seed.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserSeedService } from './user-seed.service';
 import { UserEntity } from '../../../../users/infrastructure/persistence/relational/entities/user.entity';
+import { UserFactory } from './user.factory';
+import { RoleEntity } from '../../../../roles/infrastructure/persistence/relational/entities/role.entity';
+import { StatusEntity } from '../../../../statuses/infrastructure/persistence/relational/entities/status.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UserSeedService],
-  exports: [UserSeedService],
+  imports: [TypeOrmModule.forFeature([UserEntity, RoleEntity, StatusEntity])],
+  providers: [UserSeedService, UserFactory],
+  exports: [UserSeedService, UserFactory],
 })
 export class UserSeedModule {}
