ffee15f3d50abc1f12bb7c12be90f1e918c7fec7
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2026-01-08 14:11:07 +0700

    feat: add migration to set default value for order column in multiple tables

---
+++ src/database/migrations/1767852720158-SetDefaultOrder.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SetDefaultOrder1767852720158 implements MigrationInterface {
+  name = 'SetDefaultOrder1767852720158';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "experience" ALTER COLUMN "order" SET DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "tech_stack" ALTER COLUMN "order" SET DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "portfolio" ALTER COLUMN "order" SET DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "blog" ALTER COLUMN "order" SET DEFAULT '0'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "blog" ALTER COLUMN "order" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "portfolio" ALTER COLUMN "order" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "tech_stack" ALTER COLUMN "order" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "experience" ALTER COLUMN "order" DROP DEFAULT`,
+    );
+  }
+}
